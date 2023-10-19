f3851549ba58d6ac6b2e96f69a8640bb4172e147
Егор Зимов <egor.zimow@yandex.ru>
2023-10-20 01:51:33 +0300

    reverse version 1.2

--- browser.lua
+++ browser.lua
@@ -1,14 +1,13 @@
 require "lib.moonloader"
 local sampev = require "samp.events"
-local cfg = require "inicfg"
+local JsonStatus, Json = pcall(require, 'j-cfg');
+assert(JsonStatus, 'jsoncfg.lua not found!');
 local encoding = require 'encoding'
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
 
 local server = '{EA3A50}Arizona | Faraway'
-
-local JsonStatus, Json = pcall(require, 'j-cfg')
-assert(JsonStatus, 'jsoncfg.lua not found!')
+local hasWebcore, webcore = pcall(require, 'webcore')
 
 script_url('vk.com/electronjsc')
 script_name('Browser | Arizona')
@@ -19,33 +18,71 @@ script_description('Данный скрипт был создан для полноценного использования, сер
 
 local status, cfg = Json('browser.json', {
     homepage = 'https://google.com/',
+    coords = {
+        x = 40,
+        y = 300
+    },
     window = {
         height = 190,
-        weight = 70,
+        weight = 70
     }
-});
+})
 
 
 function main()
     repeat wait(100) until isSampAvailable()
 
-    sampAddChatMessage(string.format(u8'{DBFA5F}['.. script.this.name ..'] Loaded Script. Script Authors: ' .. table.concat(script.this.authors, ', ')), -1)
-    sampAddChatMessage(string.format(u8'{DBFA5F}['.. script.this.name ..'] Description Script: ' .. script.this.description), -1)
-    
-    for i = 1, 3 do
-        sampAddChatMessage(' ', 0xFFFFFF)
+    print(cfg)
+
+    if not hasWebcore then
+        SendClientMessage('webcore.asi not loaded. Check please console for details', 0xFFFFFF)
+        print(webcore)
+        return
+    else
+        SendClientMessage('webcore.asi loaded.', 0xFFFFFF)
+        print(webcore)
     end
 
-    sampAddChatMessage(string.format(u8'{DBFA5F}['.. script.this.name ..'] {EEF425}Информация про настоящий сервер'), 0x70B95B)
-    sampAddChatMessage(string.format(u8'{DBFA5F}['.. script.this.name ..'] {EEF425}IP сервера: {55D2F7}'.. sampGetCurrentServerAddress() ..':7777 '), -1)
-    sampAddChatMessage(string.format(u8'{DBFA5F}['.. script.this.name ..'] {EEF425}Название сервера: {EA3A50}'.. sampGetCurrentServerName() ..' '), -1)
+    sampRegisterChatCommand('window', function()
+        SendClientMessage(string.format(u8'Pisition X: '.. cfg.coords.x' Position Y: '.. cfg.coords.y ..' '))
+        SendClientMessage(string.format(u8'Size W: '.. cfg.window.weight ..' Size H: 'cfg.window.height' '))
+        SendClientMessage(string.format(u8'Home Page: '.. cfg.homepage ..''))
+    end)
 
-    sampRegisterChatCommand('browser', onBrowserInitialization)
+    sampRegisterChatCommand('browser', function()
+
+        local browser = webcore:create(
+            cfg.homepage, 
+            cfg.coords.x, 
+            cfg.coords.y, 
+            cfg.window.weight, 
+            cfg.window.height
+        )
+
+        browser:set_create_cb(
+        function (_)
+            SendClientMessage('| Browser Initialization |', 0xFFFFFF)
+        end)
+
+    end)
+    
+    local state_acsess = webcore.inited() and 'Enabled' or 'Disabled'
+
+    SendClientMessage(string.format(u8'Loaded Script. Script Authors: ' .. table.concat(script.this.authors, ', ')))
+    SendClientMessage(string.format(u8'Description Script: ' .. script.this.description))
+    SendClientMessage(string.format(u8'Version Script and WebCore: '.. webcore.version() ..' | WebCore Initialization: '.. state_acsess ..' '))
+    
+    for i = 1, 3 do
+        SendClientMessage(' ', 0xFFFFFF)
+    end
+
+    SendClientMessage(string.format(u8'Information From Server'))
+    SendClientMessage(string.format(u8'IP Adress Server: {37B5F0}'.. sampGetCurrentServerAddress() ..':7777 '))
+    SendClientMessage(string.format(u8'Name Server: {EA3A50}'.. sampGetCurrentServerName() ..' '))
+
+    -- callFunction(onBrowserInitialization, 5, 5, cfg.homepage, cfg.coords.x, cfg.coords.y, cfg.window.weight, cfg.window.height)
 end
 
-
-function onBrowserInitialization()
-    
-    -- В разработке
-
+function SendClientMessage(text)
+    sampAddChatMessage(string.format('['.. script.this.name ..'] '.. text ..' '), 0xFFFFFF)
 end
